0a7778a5a0ffc7ce85e715773b62c4f8136dedc0
Fauzannur11 <muhammadnurfauzan1105@mail.ugm.ac.id>
2023-11-24 10:21:31 +0700

    update

---
+++ Kegiatan.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RPAI JOGJA</title>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css">
+    <link rel="icon" type="image/x-icon" href="Assets/img/logo.jpg">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.html">Beranda</a>
+            <a class="navbar-brand" href="Profil.php">Profil</a>
+            <a class="navbar-brand" href="Kegiatan.php">Kegiatan</a>
+            <a class="navbar-brand" href="Anggota.php">Keanggotaan dan Pengurus</a>
+            <form class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+              </form>
+        </div>
+    </nav>
+<h2>Kegiatan</h2>
+</body>
+</html>
